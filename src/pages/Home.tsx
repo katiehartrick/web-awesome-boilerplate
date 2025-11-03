2657628f1686bdb3902996e4f49e620115e78c7e
Katie H <k.hartrick@ecu.edu.au>
2025-11-03 08:26:14 +0800

    move viteLogo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { WaAnimation, WaButton, WaCallout, WaCard, WaIcon } from '@awesome.me/webawesome/dist/react'
 import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '../assets/vite.svg'
 
 function Home() {
   const [count, setCount] = useState(0)
